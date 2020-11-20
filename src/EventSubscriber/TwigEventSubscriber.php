03b50e539c73e75a03b987539373b3d5955999ab
Serge Pillay <urza45@live.fr>
2020-11-20 15:52:46 +0100

    Add Slugs

--- src/EventSubscriber/TwigEventSubscriber.php
+++ src/EventSubscriber/TwigEventSubscriber.php
@@ -2,14 +2,25 @@
 
 namespace App\EventSubscriber;
 
+use App\Repository\ConferenceRepository;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\ControllerEvent;
+use Twig\Environment;
 
 class TwigEventSubscriber implements EventSubscriberInterface
 {
+    private $twig;
+    Private $onferenceRepository;
+
+    public function __construct(Environment $twig, ConferenceRepository $onferenceRepository)
+    {
+        $this->twig = $twig;
+        $this->conferenceRepository = $onferenceRepository;
+    }
+
     public function onControllerEvent(ControllerEvent $event)
     {
-        // ...
+        $this->twig->addGlobal('conferences', $this->conferenceRepository->findAll());
     }
 
     public static function getSubscribedEvents()
